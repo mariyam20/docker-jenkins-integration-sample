35e28817fa2bbfdd06278ec7c8fc7a23bd3b3986
ARSHII <you@example.com>
2024-02-21 02:26:48 +0530

    first commit

---
+++ src/main/java/com/javatechie/example/SpringBootDockerIntegrationSampleApplication.java
@@ -0,0 +1,13 @@
+package com.javatechie.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDockerIntegrationSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDockerIntegrationSampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javatechie/example/SpringBootDockerIntegrationSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javatechie.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDockerIntegrationSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
